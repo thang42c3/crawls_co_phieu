19ae5536fd786100c516347f4b117be4b2f9839a
thang42c3 <64484789+thang42c3@users.noreply.github.com>
2021-05-10 10:45:20 +0700

    Update make_file_csv_service.py

--- app/main/service/make_file_csv_service.py
+++ app/main/service/make_file_csv_service.py
@@ -11,9 +11,24 @@ import json
 
 
 config = configs()
-basedir = os.getcwd()
 
+config = configs()
 
+def save_file(url_csv_file, rows):
+    if os.path.exists(r'{0}'.format(url_csv_file)):
+        os.remove(r'{0}'.format(url_csv_file))
+
+    with open(r'{0}'.format(url_csv_file), 'a', encoding='utf-8') as f:
+        write = csv.writer(f)
+        write.writerows(rows)
+
+def get_url(loai_so_lieu, ma_co_phieu):
+    url = config["url"][loai_so_lieu].format(ma_co_phieu)
+    url_csv_file = config["file_csv_url"][loai_so_lieu] + "_{0}".format(ma_co_phieu) + ".csv"
+    response = requests.get(url=url, proxies={'http': '', 'https': ''})
+    data = json.loads(response.content)
+    return url_csv_file, data
+'''
 def save_file(url_csv_file, rows):
 #    url_csv_file = os.path.join(basedir, 'url_csv_file')
     if os.path.exists(r'{0}'.format(url_csv_file)):
@@ -32,7 +47,7 @@ def get_url(loai_so_lieu, ma_co_phieu):
     data = json.loads(response.content)
     return url_csv_file, data
 
-
+'''
 def lich_su_gia_co_phieu(ma_co_phieu1):
     url_csv_file_bo_sung = config["file_csv_url"]["lich_su_gia_co_phieu_bo_sung"] + "_{0}.csv".format(ma_co_phieu1)
     url_csv_file = config["file_csv_url"]["lich_su_gia_co_phieu"] + "_{0}.csv".format(ma_co_phieu1)
